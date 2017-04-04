a3d930c8cc319d936cca6627d2af66131e695fcd
Sewon <sewon0613@naver.com>
2017-04-04 11:07:04 +0900

    initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,22 @@
+/**
+ * Created by danawacomputer on 2017-04-03.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        int Days = 90;
+        int hoursPerDay = 8;
+
+        int totalHours = Days * hoursPerDay;
+
+        System.out.println("총 수업시간은? " + totalHours + "시간입니다.");
+
+
+        int busCost = 1100;
+        int timesToRide = 2;
+
+        int totalBusCost = busCost * timesToRide;
+
+        System.out.println("총 버스비는? " + totalBusCost + "원입니다.");
+    }
+}
---
+++ src/MyAccountBook.java
@@ -0,0 +1,16 @@
+/**
+ * Created by danawacomputer on 2017-04-03.
+ */
+public class MyAccountBook {
+    public static void main(String[] args) {
+
+        int busFee = 1100;
+        int forLunch = 7500;
+        int icedChocolatte = 2900;
+
+        int totalSpent = busFee + forLunch + icedChocolatte;
+
+        System.out.println("총 경비는 얼마입니까? " + totalSpent + "원입니다.");
+    }
+
+}
